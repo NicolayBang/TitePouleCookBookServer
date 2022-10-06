bfe9a84656345ea400dcd541e82f5064927ca2cb
NicolayBang <nicolaybang@gmail.com>
2022-10-06 18:23:50 -0400

    Made connection as class final variable initialized at launch (removed connectToDB method).
    Made connection as class final variable initialized at launch (removed connectToDB method).

--- src/main/java/com/example/RecipeBook/RestController/RecipeBookControllerImpl.java
+++ src/main/java/com/example/RecipeBook/RestController/RecipeBookControllerImpl.java
@@ -21,7 +21,7 @@ import java.sql.SQLException;
 public class RESTControllerImpl implements RestListener, RestController {
 
     private static final DBWriter dbWriter = new DBWriter();
-  //  private static final RESTControllerImpl recipeHandler = new RESTControllerImpl();
+    private static final RESTControllerImpl recipeHandler = new RESTControllerImpl();
 
 
     public static final Connection connection;
@@ -40,8 +40,8 @@ public class RESTControllerImpl implements RestListener, RestController {
 //        connectToDB();
 //    }
 
-    public RESTControllerImpl getInstance() {
-        return this;
+    public static RESTControllerImpl getInstance() {
+        return recipeHandler;
     }
     private long connections = 0;
 
